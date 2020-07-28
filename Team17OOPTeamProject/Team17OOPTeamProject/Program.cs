a9d35b8c5bdd2c481aa6cc021940f0a3f0b91f66
Nik Nikolov <nnickolov1@gmail.com>
2020-07-28 22:44:39 +0300

    Team 17 project created!

---
+++ Team17OOPTeamProject/Team17OOPTeamProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Team17OOPTeamProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
