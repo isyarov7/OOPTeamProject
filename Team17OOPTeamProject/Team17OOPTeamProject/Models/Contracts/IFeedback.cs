789dd59721da321110fe67f444aa018b1075b28a
Nik Nikolov <nnickolov1@gmail.com>
2020-07-29 00:32:46 +0300

    New Feedback class was created. New Interface was created.

---
+++ Team17OOPTeamProject/Team17OOPTeamProject/Models/Contracts/IFeedback.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Team17OOPTeamProject.Models.Enums;
+
+namespace Team17OOPTeamProject.Models.Contracts
+{
+    public interface IFeedback
+    {
+        int Rating { get; }
+        FeedbackStatus FeedbackStatus { get; }
+    }
+}
---
+++ Team17OOPTeamProject/Team17OOPTeamProject/Models/Feedback.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Team17OOPTeamProject.Models.Abstract;
+using Team17OOPTeamProject.Models.Contracts;
+using Team17OOPTeamProject.Models.Enums;
+
+namespace Team17OOPTeamProject
+{
+    public class Feedback : Abstract, IFeedback
+    {
+        //Fields
+        private int rating;
+        FeedbackStatus feedbackStatus;
+        //Constructor
+        public Feedback(string title, string description, int rating, FeedbackStatus feedbackStatus, List<string> comments, List<string> history)
+            : base(title, description, comments, history)
+        {
+            this.Rating = rating;
+            this.FeedbackStatus = feedbackStatus;
+        }
+        //Properties
+        public int Rating
+        {
+            get => this.rating;
+            set => this.rating = value;
+        }
+
+        public FeedbackStatus FeedbackStatus { get => this.feedbackStatus; set => feedbackStatus = value;}
+    }
+}
