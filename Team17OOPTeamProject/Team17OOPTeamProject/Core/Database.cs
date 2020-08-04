60b1b5dc91551f8548388ce78363967c588bfcd2
Nik Nikolov <nnickolov1@gmail.com>
2020-08-04 16:26:32 +0300

    Started to implementing Database class

--- Team17OOPTeamProject/Team17OOPTeamProject/Core/Database.cs
+++ Team17OOPTeamProject/Team17OOPTeamProject/Core/Database.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using T17.Models.Core.Contracts;
 
 namespace T17.Models.Core
 {
-    class Database
+    public class Database : IDatabase
     {
+        private static IDatabase instance = null;
+        public static IDatabase Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Database();
+                }
+
+                return instance;
+            }
+        }
+
+        private readonly List<IVehicle> vehicles = new List<IVehicle>();
+        public IList<IVehicle> Vehicles
+        {
+            get => this.vehicles;
+        }
+
+        private readonly List<IJourney> journeys = new List<IJourney>();
+        public IList<IJourney> Journeys
+        {
+            get => this.journeys;
+        }
+
+        private readonly List<ITicket> tickets = new List<ITicket>();
+        public IList<ITicket> Tickets
+        {
+            get => this.tickets;
+        }
     }
 }
