e8930d3668509589501b57494a8caa0714e7ea80
Iliyan Syarov <isyarov98@gmail.com>
2020-08-09 18:44:33 +0300

    BugTest was created.

---
+++ Team17OOPTeamProject/WIM.T17.Tests/BugTests/BugTest.cs
@@ -0,0 +1,141 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using T17.Models.Models;
+using Team17OOPTeamProject.Models;
+using Team17OOPTeamProject.Models.Abstract;
+using Team17OOPTeamProject.Models.Enums;
+
+namespace WIM.T17.Tests.BugTests
+{
+    class BugTest
+    {
+        [TestMethod]
+        public void HasTitle()
+        {
+            Bug bug = new Bug("BugTitlefsdfasdfasdf", new List<string> (){"dasmdkasmd"}, Priority.High, Severity.Critical, BugStatus.Active, new List<string>() {"dasdasdas"});
+
+            string expected = "BugTitlefsdfasdfasdf";
+            string actual = bug.Title;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsAdvanceStatusCorrect()
+        {
+            Bug bug = new Bug("Ttile", new List<string>() { "dasmdkasmd" }, Priority.High, Severity.Critical, BugStatus.Active, new List<string>() { "dasdasdas" });
+
+            BugStatus expected = BugStatus.Fixed;
+            BugStatus actual = bug.BugStatus;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void IsLowerStatusCorrect()
+        {
+            Bug bug = new Bug("Ttile", new List<string>() { "dasmdkasmd" }, Priority.High, Severity.Critical, BugStatus.Active, new List<string>() { "dasdasdas" });
+
+            BugStatus expected = BugStatus.Active;
+            BugStatus actual = bug.BugStatus;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod]
+        public void StepsToReproduce()
+        {
+            Bug bug = new Bug("Ttile", new List<string>() { "dasmdkasmd" }, Priority.High, Severity.Critical, BugStatus.Active, new List<string>() { "dasdasdas" });
+
+            List<string> expected = new List<string>() { "dasdasdas" };
+            List<string> actual = bug.StepsToProduce;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void HasPriority()
+        {
+            Bug bug = new Bug("Ttile", new List<string>() { "dasmdkasmd" }, Priority.High, Severity.Critical, BugStatus.Active, new List<string>() { "dasdasdas" });
+
+            Priority expected = Priority.High;
+            Priority actual = bug.Priority;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void HasSeverity()
+        {
+            Bug bug = new Bug("Ttile", new List<string>() { "dasmdkasmd" }, Priority.High, Severity.Critical, BugStatus.Active, new List<string>() { "dasdasdas" });
+
+            Severity expected = Severity.Critical;
+            Severity actual = bug.Severity;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddsAssignee()
+        {
+            Bug bug = new Bug("Ttile", new List<string>() { "dasmdkasmd" }, Priority.High, Severity.Critical, BugStatus.Active, new List<string>() { "dasdasdas" });
+            Member member = new Member("pesho");
+
+            bug.AddAssignee(member);
+
+            Assert.IsTrue(bug.Assignee.Contains(member));
+        }
+
+        [TestMethod]
+        public void AddsUniqueAssignee()
+        {
+            Bug bug = new Bug("Ttile", new List<string>() { "dasmdkasmd" }, Priority.High, Severity.Critical, BugStatus.Active, new List<string>() { "dasdasdas" });
+            Member member = new Member("Gosho");
+
+            bug.AddAssignee(member);
+
+            string expected = $"Member {member.Name} is already on the list!";
+            string actual = bug.AddAssignee(member);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+
+        public void RemoveAssignee()
+        {
+            Bug bug = new Bug("Ttile", new List<string>() { "dasmdkasmd" }, Priority.High, Severity.Critical, BugStatus.Active, new List<string>() { "dasdasdas" });
+            Member member = new Member("Pesho");
+
+            bug.RemoveAssignee(member);
+
+            Assert.IsTrue(!bug.Assignee.Contains(member));
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        public void ThrowsExceptionWhenTitleOOB()
+        {
+            Bug bug = new Bug("asdnklndklasdklnaskldnklasndkasndkansdnalsn", new List<string>() { "dasmdkasmd" }, Priority.High, Severity.Critical, BugStatus.Active, new List<string>() { "dasdasdas" });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ThrowsExceptionWhenTitleNull()
+        {
+            Bug bug = new Bug("", new List<string>() { "dasmdkasmd" }, Priority.High, Severity.Critical, BugStatus.Active, new List<string>() { "dasdasdas" });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ThrowsExceptionWhenDescriptionOOB()
+        {
+           Bug bug = new Bug("Ttile", new List<string>() { "dsa" }, Priority.High, Severity.Critical, BugStatus.Active, new List<string>() { "dasdasdas" });
+
+        }
+    }
+}
