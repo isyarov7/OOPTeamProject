d18cb851554acb376f5067b2ff09c9e4f91f55d3
Iliyan Syarov <isyarov98@gmail.com>
2020-08-10 22:46:03 +0300

    CreateStorryCommand created.

---
+++ Team17OOPTeamProject/Team17OOPTeamProject/Commands/CreateNewBoardInTeamCommand.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WIM.T17.Commands
+{
+    class CreateNewBoardInTeamCommand
+    {
+        //TODO
+    }
+}
---
+++ Team17OOPTeamProject/Team17OOPTeamProject/Commands/CreateNewStoryCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using T17.Models.Commands.Abstracts;
+
+namespace WIM.T17.Commands
+{
+    public class CreateNewStoryCommand : Command
+    {
+        public CreateNewStoryCommand(IList<string> commandParameters)
+           : base(commandParameters)
+        {
+        }
+
+        public override string Execute()
+        {
+
+            if (CommandParameters.Count < 5)
+                throw new ArgumentException("You have to submit 6 parameters!");
+
+            string title = this.CommandParameters[0];
+            List<string> description = this.CommandParameters[1].Trim().Split(',').ToList();
+            Enum.TryParse<Priority>(this.CommandParameters[2], true, out Priority priority);
+            Enum.TryParse<Severity>(this.CommandParameters[3], true, out Severity severity);
+            Enum.TryParse<BugStatus>(this.CommandParameters[4], true, out BugStatus bugStatus);
+            List<string> stepsToProduce = this.CommandParameters[5].Trim().Split(',').ToList();
+
+            var bug = this.Factory.CreateBug(title, description, priority, severity, bugStatus, stepsToProduce);
+            this.Database.Bugs.Add(bug);
+
+            bug.History.Add($"Bug with title: {title} was created!");
+
+            return $"Bug with ID {this.Database.Bugs.Count} was created.";
+        }
+    }
+}
