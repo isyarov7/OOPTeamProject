a76612e5ad268e2a7e563556b46404ece29b2155
Nik Nikolov <nnickolov1@gmail.com>
2020-08-15 17:40:45 +0300

    Cheating Singleton BaseTestClass made. New AddBugToBoardCommand test was made!

---
+++ Team17OOPTeamProject/WIM.T17.Tests/BaseTestClass.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using T17.Models.Core;
+using T17.Models.Core.Contracts;
+
+namespace WIM.T17.Tests
+{
+    public class BaseTestClass
+    {
+        protected IDatabase database = Database.Instance;
+
+        [TestInitialize]
+        public void ResetState()
+        {
+            var instanceField = typeof(Database).GetField("instance", BindingFlags.NonPublic | BindingFlags.Static);
+            var newSingleton = new Database();
+            database = newSingleton;
+            instanceField.SetValue(newSingleton, newSingleton);
+        }
+    }
+}
--- Team17OOPTeamProject/WIM.T17.Tests/CommandsTests_Should/AddCommandTests_Should/AddCommand_Should.cs
+++ Team17OOPTeamProject/WIM.T17.Tests/CommandsTests_Should/AddCommandTests_Should/AddCommand_Should.cs
@@ -4,45 +4,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading.Tasks.Sources;
 using T17.Models.Core;
 using T17.Models.Core.Contracts;
 using T17.Models.Models;
+using T17.Models.Models.Contracts;
 using Team17OOPTeamProject.Models;
+using Team17OOPTeamProject.Models.Contracts;
 using Team17OOPTeamProject.Models.Enums;
 using WIM.T17.Commands;
 
 namespace WIM.T17.Tests.CommandsTests_Should.AddCommandTests_Should
 {
     [TestClass]
-    public class AddCommand_Should
+    public class AddCommand_Should : BaseTestClass
     {
         [TestMethod]
-        public void AddsBugToBoardCommand_Should() 
+        public void AddsBugToBoardCommand_Should()
         {
-            IDatabase database = new Database();
-            IFactory factory = new Factory();
+            string teamName = "Tigrite";
+            ITeam team = new Team(teamName);
 
             string boardName = "Board";
-            var board = new Board(boardName);
+            IBoard board = new Board(boardName);
 
             string bugTitle = "MnogoLoshBug";
             string description = "Bug description";
-            string stepsToProduce = "steps to produce";
             Priority priority = Priority.High;
             Severity severity = Severity.Critical;
-            var bug = new Bug(bugTitle, description, priority, severity, stepsToProduce);
+            string stepsToProduce = "steps to produce";
+            IBug bug = new Bug(bugTitle, description, priority, severity, stepsToProduce);
+
+            database.Teams.Add(team);
             database.Boards.Add(board);
+            team.Boards.Add(board);
             database.Bugs.Add(bug);
+
             List<string> parameters = new List<string>
-            {                
+            {
                 bugTitle,
                 boardName
             };
+
             AddBugToBoardCommand command = new AddBugToBoardCommand(parameters);
             command.Execute();
-            Assert.IsTrue(board.WorkItems.Contains(bug));
-            
-            
+            Assert.IsTrue(board.WorkItems.Any(x => x.Title == bugTitle));
         }
     }
 }
